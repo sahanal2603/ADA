11b633f00df8086b65f0f033b92d81f19041593e
sahanal2603 <53811265+sahanal2603@users.noreply.github.com>
2020-02-11 11:46:28 +0530

    Update 1binarysearch.cpp

--- 1binarysearch.cpp
+++ 1binarysearch.cpp
@@ -1,4 +1,4 @@
-// C++ program to implement binary search in sorted array 
+
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -6,7 +6,7 @@ int binarySearch(int arr[], int low, int high, int key)
 { 
     if (high < low) 
         return -1; 
-    int mid = (low + high) / 2; /*low + (high - low)/2;*/
+    int mid = (low + high) / 2;
     if (key == arr[mid]) 
         return 1; 
     if (key > arr[mid]) 
@@ -14,19 +14,17 @@ int binarySearch(int arr[], int low, int high, int key)
     return binarySearch(arr, low, (mid - 1), key); 
 } 
   
-/* Driver code */
+
 int main() 
 { 
-    // Let us search 3 in below array 
-   // int arr[100] = { 5, 6, 7, 8, 9, 10 }; 
     int N, K,index,T,i,j; 
-   // N = sizeof(arr) / sizeof(arr[0]); 
+  
     cin>>T; 
     for(j=1;j<=T;j++)
     {
 	
     cin>>N;
-   // cout<<" ";
+   
     cin>>K;
     int arr[N];
     for(i=0;i<N;i++)
